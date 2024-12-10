f0a693faf272625220ea33079fb6d74fc8878168
Marci-420 <meszaros.marcell.zsolt.2020i@bankitatabanya.hu>
2024-12-10 08:22:51 +0100

    Kereső rész legördülő listával és navbar hozzáadása

--- vueProject/src/App.vue
+++ vueProject/src/App.vue
@@ -8,9 +8,55 @@ var receptek = ref([recipes]);
 <template>
 
   <header>
+    <section>
 
-  </header>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">Receptkönyv</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Receptek</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Új recept</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kedvencek</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+
+</section>
+
+</header>
+
   <main>
+
+  <section>
+      <div class="d-flex align-items-center">
+        <div class="searchBar form-outline flex-fill w-50" data-mdb-input-init>
+          <input type="search" id="form1" class="form-control" placeholder="Keress receptet..." aria-label="Search" />
+        </div>
+        <select class=" select1 form-select w-25" aria-label="Default select example">
+          <option selected>Minden nehézség</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        <select class="select2 form-select w-25" aria-label="Default select example">
+          <option selected>Elkészítési idő szerint</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+    </section>
 
     <section>
 
@@ -44,5 +90,15 @@ var receptek = ref([recipes]);
 <style scoped>
 span{
   margin-bottom: 25px;
+}
+
+.searchBar{
+  margin-top: 25px;
+  margin-bottom: 25px;
+}
+
+.select1{
+  margin-left: 20px;
+  margin-right: 20px;
 }
 </style>
