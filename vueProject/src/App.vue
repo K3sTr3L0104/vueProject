3e28720379a03b9f4d81adddef6a9ea07ecc1e16
K3sTr3L0104 <84494408+K3sTr3L0104@users.noreply.github.com>
2024-12-10 09:19:09 +0100

    komponensbe átrakva az objektumlista és a megjelenítés, illetve keresés megoldása + sort system

--- vueProject/src/App.vue
+++ vueProject/src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import { ref } from 'vue';
-import { recipes } from '../data/dummyData';
-var receptek = ref([recipes]);
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -10,94 +8,53 @@ var receptek = ref([recipes]);
   <header>
     <section>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-<div class="container-fluid">
-  <a class="navbar-brand" href="#">Receptkönyv</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Receptek</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Új recept</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Kedvencek</a>
-      </li>
-    </ul>
-  </div>
-</div>
-</nav>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Receptkönyv</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Receptek</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" @click="newPage()">Új recept</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Kedvencek</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-</section>
+    </section>
 
-</header>
-
+  </header>
   <main>
-
-  <section>
-      <div class="d-flex align-items-center">
-        <div class="searchBar form-outline flex-fill w-50" data-mdb-input-init>
-          <input type="search" id="form1" class="form-control" placeholder="Keress receptet..." aria-label="Search" />
-        </div>
-        <select class=" select1 form-select w-25" aria-label="Default select example">
-          <option selected>Minden nehézség</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-        <select class="select2 form-select w-25" aria-label="Default select example">
-          <option selected>Elkészítési idő szerint</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-      </div>
-    </section>
-
-    <section>
-
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col" v-for="recipe in recipes">
-        <div class="card h-100">
-          <img :src="recipe.imageUrl" class="card-img-top" alt="...">        
-          <div class="card-body">
-            <h5 class="card-title">{{recipe.name}}</h5>
-            <p class="card-text">Elkészítési idő: {{ recipe.cookTime }} perc</p>
-            <div>
-            <span class="badge" :class="{ 'bg-success': recipe.difficulty === 'könnyű', 'bg-warning': recipe.difficulty === 'közepes', 'bg-danger': recipe.difficulty === 'nehéz' }">{{ recipe.difficulty }}</span>
-          </div>
-            <button class="btn btn-primary" type="submit">Button</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    </section>
-
     <HelloWorld/>
   </main>
   <footer>
 
   </footer>
-  
-  
+
+
 </template>
 
 <style scoped>
-span{
+span {
   margin-bottom: 25px;
 }
 
-.searchBar{
+.searchBar {
   margin-top: 25px;
   margin-bottom: 25px;
 }
 
-.select1{
+.select1 {
   margin-left: 20px;
   margin-right: 20px;
 }
--- vueProject/src/components/HelloWorld.vue
+++ vueProject/src/components/HelloWorld.vue
@@ -1,11 +1,97 @@
 <script setup>
+import { recipes } from '../../data/dummyData';
+import { ref, watch } from 'vue';
 
+var chose = ref("Elkészítési idő szerint");
+var difficultyFilter = ref("Minden nehézség");
+var selectedItems = ref([...recipes]);
+var searchValue = ref("");
+
+watch([chose, difficultyFilter, searchValue], () => {
+  let filteredRecipes = [...recipes];
+  if (searchValue.value) {
+    filteredRecipes = filteredRecipes.filter(recipe => 
+      recipe.name.toLowerCase().includes(searchValue.value.toLowerCase())
+    );
+  }
+
+  if (difficultyFilter.value !== "Minden nehézség") {
+    filteredRecipes = filteredRecipes.filter(recipe => recipe.difficulty === difficultyFilter.value);
+  }
+
+  if (chose.value === "növekvő") {
+    filteredRecipes.sort((a, b) => a.cookTime - b.cookTime);
+  } else if (chose.value === "sort") {
+    filteredRecipes.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  selectedItems.value = filteredRecipes;
+  console.log(selectedItems.value);
+});
 </script>
 
+
 <template>
- 
+    <main>
+  
+      <section>
+        <div class="d-flex align-items-center">
+          <div class="searchBar form-outline flex-fill w-50" data-mdb-input-init>
+            <input type="search" id="form1" class="form-control" placeholder="Keress receptet..." aria-label="Search" v-model="searchValue" />
+          </div>
+
+          <select class="select1 form-select w-25" aria-label="Default select example" v-model="difficultyFilter">
+            <option value="Minden nehézség">Minden nehézség</option>
+            <option value="könnyű">Könnyű</option>
+            <option value="közepes">Közepes</option>
+            <option value="nehéz">Nehéz</option>
+          </select>
+
+          <select class="select2 form-select w-25" aria-label="Default select example" v-model="chose">
+            <option value="Elkészítési idő szerint">Elkészítési idő szerint</option>
+            <option value="növekvő">Növekvő</option>
+            <option value="sort">ABC Sorrend</option>
+          </select>
+        </div>
+      </section>
+  
+      <section>
+  
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="col" v-for="recipe in selectedItems" :key="recipe.name">
+            <div class="card h-100">
+              <img :src="recipe.imageUrl" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">{{ recipe.name }}</h5>
+                <p class="card-text">Elkészítési idő: {{ recipe.cookTime }} perc</p>
+                <div>
+                  <span class="badge"
+                    :class="{ 'bg-success': recipe.difficulty === 'könnyű', 'bg-warning': recipe.difficulty === 'közepes', 'bg-danger': recipe.difficulty === 'nehéz' }">
+                    {{ recipe.difficulty }}
+                  </span>
+                </div>
+                <button class="btn btn-primary" type="submit">Részletek</button>
+              </div>
+            </div>
+          </div>
+        </div>
+  
+      </section>
+    </main>
 </template>
 
 <style scoped>
+span {
+  margin-bottom: 25px;
+}
 
+.searchBar {
+  margin-top: 25px;
+  margin-bottom: 25px;
+}
+
+.select1 {
+  margin-left: 20px;
+  margin-right: 20px;
+}
 </style>
